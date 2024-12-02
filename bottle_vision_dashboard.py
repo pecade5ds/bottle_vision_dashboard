1a3a943bc285849d5cc0f481462603b177fe2d3f
Pecade <pedro.cadahia@5digitalstreet.com>
2024-12-02 18:23:59 +0100

    Update bottle_vision_dashboard.py

--- bottle_vision_dashboard.py
+++ bottle_vision_dashboard.py
@@ -11,29 +11,31 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import requests
 
-st.title("Dashboard de Ejemplo")
+st.title("Bottle Vision Dashboard")
 
 # Firebase credentials
 db = firestore.Client.from_service_account_info(st.secrets["firebase"])
 
+# Config load
 with open('./Data/config/query_config.json', 'r') as json_file:
     db_schema_name_str = json.load(json_file)["db_schema_name"]
 
+# Competitor info load
 with open('./Data/competitor_danone_labels_dict.json', 'r') as json_file:
     competitor_danone_labels_dict = json.load(json_file)
 
+# Firebase conection
 docs = db.collection(db_schema_name_str).get()
 
+# Preprocessing Firebase info
 df_docs = preprocess_docs(docs, competitor_danone_labels_dict)
 
-# gross salary
+# External Info: gross salary
 gross_salary_postcode_df = pd.read_csv("./Data/renta_barcelona.csv", sep=";", decimal=",")
 gross_salary_postcode_df['Cat_avg_Gross_Income'] = pd.qcut(gross_salary_postcode_df['Average Gross Income'], q=3, labels=['Low', 'Medium', 'High'])
 gross_salary_postcode_df['Cat_avg_Disposable_Income'] = pd.qcut(gross_salary_postcode_df['Average Disposable Income'], q=3, labels=['Low', 'Medium', 'High'])
 
-# Post codes
-
-
+# Post codes geojson load
 gdf_post_code = gpd.read_file('./Data/BARCELONA.geojson')
 
 gdf_post_code = gdf_post_code.merge(
@@ -47,8 +49,8 @@ gdf_post_code = gdf_post_code.merge(
 post_code_data  = df_docs[df_docs.columns.intersection(list(competitor_danone_labels_dict.keys()) + ["post_code",'total_danone', 'total_non_danone', 'total_bottles'])].groupby("post_code").sum().reset_index()
 gdf_post_code = gdf_post_code.merge(post_code_data, left_on="COD_POSTAL", right_on="post_code",how="left").drop(["ID_CP","post_code","ALTA_DB","ZIP_code","CODIGO_INE"] ,axis=1)
 
-# Crear las pestañas
-tabs = st.sidebar.radio("Selecciona una pestaña", ("Datos Generales", "Datos Geolocalizados"))
+
+tabs = st.radio("Selecciona una pestaña", ("Datos Generales", "Datos Geolocalizados"))
 
 if tabs == "Datos Generales":
     st.header("Datos Generales")
